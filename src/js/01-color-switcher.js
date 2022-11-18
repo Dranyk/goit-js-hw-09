bea18f7d957d3d95984c4f1866855e234436e85c
Dranyk <dvdranyk@gmail.com>
2022-11-18 18:38:12 +0200

    1- done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener("click", evt => {
+    evt.target.setAttribute('disabled', 'disabled');
+  timerId = setInterval(() => {
+ document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", evt => {
+    startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
